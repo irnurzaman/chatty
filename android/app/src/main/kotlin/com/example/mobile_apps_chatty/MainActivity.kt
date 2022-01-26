3cf21ddec73b75433af3d210bc9d3a86b7d94780
Ikhwan Rizqy Nurzaman <ikhwanrnurzaman@gmail.com>
2022-01-26 20:23:22 +0700

    chore: Boilerplate

---
+++ android/app/src/main/kotlin/com/example/mobile_apps_chatty/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_apps_chatty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [Image.asset("name")],
+          )
+        ],
+      ),
+    );
+  }
+}
