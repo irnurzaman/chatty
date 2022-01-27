70464ef9335454b1f39ac01f3d094be65f4b571c
Ikhwan Rizqy Nurzaman <ikhwanrnurzaman@gmail.com>
2022-01-27 07:59:50 +0700

    chore: Add color and font theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_apps_chatty/home.dart';
+import 'package:mobile_apps_chatty/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blue = const Color(0xff1F8DF5);
+Color ashBlue = const Color(0xff9ED0FF);
+Color white = const Color(0xffFFFFFF);
+Color black = const Color(0xff2C3A59);
+Color lightGrey = const Color(0xff808BA2);
+Color grey = const Color(0xffEBEFF3);
+Color darkGrey = const Color(0xff505C6B);
+
+TextStyle title = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500, color: black);
+TextStyle subtitle = GoogleFonts.poppins(fontWeight: FontWeight.w300, color: lightGrey);
+TextStyle avaName = GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w500, color: white);
+TextStyle avaTitle = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w300, color: ashBlue);
+TextStyle chat = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w300, color: darkGrey);
+TextStyle timestamp = GoogleFonts.poppins(fontWeight: FontWeight.w300, color: lightGrey);
