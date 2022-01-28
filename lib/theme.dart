4caee6abdfb131bbc218acd7453941a26b4ed013
Ikhwan Rizqy Nurzaman <ikhwanrnurzaman@gmail.com>
2022-01-28 22:02:37 +0700

    feat: Add floatingActionButton

--- lib/theme.dart
+++ lib/theme.dart
@@ -8,6 +8,7 @@ Color black = const Color(0xff2C3A59);
 Color lightGrey = const Color(0xff808BA2);
 Color grey = const Color(0xffEBEFF3);
 Color darkGrey = const Color(0xff505C6B);
+Color green = const Color(0xff29CB9E);
 
 TextStyle title = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500, color: black);
 TextStyle subtitle = GoogleFonts.poppins(fontWeight: FontWeight.w300, color: lightGrey);
